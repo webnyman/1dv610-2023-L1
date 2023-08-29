01a578c4bfae8261ce13f610bb91e6bf18e50729
webnyman <bjorn.nyman@webflex.se>
2023-08-29 11:00:55 +0200

    Add rovarsprak translation function

--- javascript/rovare.js
+++ javascript/rovare.js
@@ -3,6 +3,21 @@ const submitButton = document.querySelector('#submit')
 
 submitButton.addEventListener('click', () => {
   const name = textInputForName.value
-  const greeting = `Hej ${name}!`
-  console.log(greeting)
+  const greeting = 'Hej'
+  const greetingRovarSprak = translateToRovarSprak(greeting)
+  const namnRovarsprak = translateToRovarSprak(name)
+  console.log(greetingRovarSprak + ' ' + namnRovarsprak)
 })
+
+const translateToRovarSprak = (textToTranslate) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'å', 'ä', 'ö']
+  let rovarSprak = ''
+  for (const letter of textToTranslate) {
+    if (vowels.includes(letter)) {
+      rovarSprak += letter
+    } else {
+      rovarSprak += letter + 'o' + letter
+    }
+  }
+  return rovarSprak
+}
